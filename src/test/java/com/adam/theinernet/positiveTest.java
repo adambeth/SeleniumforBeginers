7c8712383c438ddcde15188c0d4e5f4072ccc32c
beth <adam.bnethlehem1@gmail.com>
2019-08-18 23:08:52 +0200

    fix WebDriverManger implementation

--- src/test/java/com/adam/theinernet/positiveTest.java
+++ src/test/java/com/adam/theinernet/positiveTest.java
@@ -6,29 +6,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 @Test
 public class positiveTest {
-    WebDriver driver;
+
+    private WebDriver driver;
+
     @BeforeClass
-    public void startChrome(){
+    public void setupClass() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
     }
 
     public void loginTest(){
+
         String url = "http://the-internet.herokuapp.com/login";
         driver.get(url);
         driver.manage().window().maximize();
         driver.findElement(By.xpath("/html//input[@id='username']")).sendKeys("adam");
         driver.findElement(By.xpath("/html//input[@id='password']")).sendKeys("Password");
         driver.findElement(By.xpath("//form[@id='login']/button[@class='radius']")).click();
+
     }
 
     @AfterTest
     public void tearDown(){
+
         driver.quit();
 
     }
