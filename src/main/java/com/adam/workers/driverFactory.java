7336f42ed0d2c24d60bdfd104ec1632c671f38cf
beth <adam.bnethlehem1@gmail.com>
2019-08-21 08:14:23 +0200

    driverFactory - <message: Working Base>

--- src/main/java/com/adam/workers/driverFactory.java
+++ src/main/java/com/adam/workers/driverFactory.java
@@ -33,34 +33,4 @@ public class driverFactory {
         return driver.get();
     }
 }
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//
-//public class driverFactory {
-//    private ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
-//
-//    public WebDriver driverSelect(String browser){
-//
-//        switch (browser.toLowerCase()){
-//
-//            case "chrome":
-//                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-//                driver.set(new ChromeDriver());
-//                break;
-//            case "firefox":
-//                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-//                driver.set(new FirefoxDriver());
-//                break;
-//
-//            default:
-//                throw new IllegalStateException("Unexpected value: " + browser.toLowerCase());
-//        }
-//        return driver;
-//
-//    }
-//}
-//
+
